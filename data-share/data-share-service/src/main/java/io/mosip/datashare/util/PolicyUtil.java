d9d5bfe625727fdee6660355061c35db5f0191e1
M1022006 <M1022006@A2ML18485.mindtree.com>
2020-09-02 13:12:25 +0530

    MOSIP-8289 policy api updated

--- data-share/data-share-service/src/main/java/io/mosip/datashare/util/PolicyUtil.java
+++ data-share/data-share-service/src/main/java/io/mosip/datashare/util/PolicyUtil.java
@@ -1,51 +1,107 @@
 package io.mosip.datashare.util;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import io.mosip.datashare.dto.DataSharePolicies;
-import io.mosip.datashare.dto.Policies;
-import io.mosip.datashare.dto.PolicyDetailResponseDto;
-import io.mosip.datashare.dto.ShareableAttribute;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.mosip.datashare.constant.LoggerFileConstant;
+import io.mosip.datashare.dto.PolicyManagerResponseDto;
+import io.mosip.datashare.dto.PolicyResponseDto;
+import io.mosip.datashare.exception.ApiNotAccessibleException;
+import io.mosip.datashare.exception.PolicyException;
+import io.mosip.datashare.logger.DataShareLogger;
+import io.mosip.kernel.core.exception.ExceptionUtils;
+import io.mosip.kernel.core.exception.ServiceError;
+import io.mosip.kernel.core.logger.spi.Logger;
 
 @Component
 public class PolicyUtil {
 
 
-	public PolicyDetailResponseDto getPolicyDetail(String policyId, String subscriberId) {
-		// TODO call REST api of partner management
-		// Now its Mocked to give PolicyDetailResponse
-		PolicyDetailResponseDto policyDetailResponseDto = new PolicyDetailResponseDto();
-		policyDetailResponseDto.setId("45678451034176");
-		policyDetailResponseDto.setVersion("1.1");
-		policyDetailResponseDto.setName("Digital QR Code Policy");
-		policyDetailResponseDto.setDesc("");
-		DataSharePolicies dataSharePolicies = new DataSharePolicies();
-		dataSharePolicies.setEncryptionType("partnerBased");
-		dataSharePolicies.setShareDomain("dev.mosip.net");
-		dataSharePolicies.setTransactionsAllowed(2);
-		dataSharePolicies.setValidForInMinutes(60);
-		Policies policies = new Policies();
-		policies.setDataSharePolicies(dataSharePolicies);
-		List<ShareableAttribute> sharableAttributesList = new ArrayList<ShareableAttribute>();
-		ShareableAttribute shareableAttribute1 = new ShareableAttribute();
-		shareableAttribute1.setAttributeName("fullName");
-		shareableAttribute1.setEncrypted(true);
-		sharableAttributesList.add(shareableAttribute1);
-		ShareableAttribute shareableAttribute2 = new ShareableAttribute();
-		shareableAttribute2.setAttributeName("dateOfBirth");
-		shareableAttribute2.setEncrypted(true);
-		sharableAttributesList.add(shareableAttribute2);
-		ShareableAttribute shareableAttribute3 = new ShareableAttribute();
-		shareableAttribute3.setAttributeName("face");
-		shareableAttribute3.setEncrypted(true);
-		shareableAttribute3.setFormat("extraction");
-		sharableAttributesList.add(shareableAttribute3);
-		policies.setShareableAttributes(sharableAttributesList);
-		policyDetailResponseDto.setPolicies(policies);
-		return policyDetailResponseDto;
+
+	/** The env. */
+	@Autowired
+	private Environment env;
+
+	/** The rest template. */
+	@Autowired
+	private RestTemplate restTemplate;
+
+	/** The cryptomanager encrypt url. */
+	@Value("${PARTNER_POLICY}")
+	private String partnerPolicyUrl;
+
+	private static final Logger LOGGER = DataShareLogger.getLogger(PolicyUtil.class);
+
+	/** The mapper. */
+	@Autowired
+	private ObjectMapper mapper;
+	public PolicyResponseDto getPolicyDetail(String policyId, String subscriberId) {
+
+		try {
+			LOGGER.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.SUBSCRIBERID.toString(),
+					LoggerFileConstant.SUBSCRIBERID.toString(), "PolicyUtil::getPolicyDetail()::entry");
+		String uri = partnerPolicyUrl;
+		Map<String, String> parameters = new HashMap<>();
+		parameters.put("partnerId", subscriberId);
+		parameters.put("policyId", policyId);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath(uri);
+		builder.build(parameters);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity httpEntity = new HttpEntity<>(headers);
+		ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, httpEntity,
+				String.class);
+			PolicyManagerResponseDto responseObject;
+
+			responseObject = mapper.readValue(response.getBody(), PolicyManagerResponseDto.class);
+			if (responseObject != null && responseObject.getErrors() != null && !responseObject.getErrors().isEmpty()) {
+				ServiceError error = responseObject.getErrors().get(0);
+				throw new PolicyException(error.getMessage());
+			}
+			PolicyResponseDto policyResponseDto = responseObject.getResponse();
+			LOGGER.info(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.SUBSCRIBERID.toString(),
+					subscriberId,
+					"Fetched policy details successfully");
+			LOGGER.debug(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.SUBSCRIBERID.toString(),
+					LoggerFileConstant.SUBSCRIBERID.toString(), "PolicyUtil::getPolicyDetail()::exit");
+			return policyResponseDto;
+		} catch (IOException e) {
+			LOGGER.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.POLICYID.toString(),
+					LoggerFileConstant.POLICYID.toString(),
+					"PolicyUtil::getPolicyDetail():: error with error message" + ExceptionUtils.getStackTrace(e));
+			throw new PolicyException(e);
+		} catch (Exception e) {
+			LOGGER.error(LoggerFileConstant.SESSIONID.toString(), LoggerFileConstant.POLICYID.toString(),
+					LoggerFileConstant.POLICYID.toString(),
+					"PolicyUtil::getPolicyDetail():: error with error message" + ExceptionUtils.getStackTrace(e));
+			if (e instanceof HttpClientErrorException) {
+				HttpClientErrorException httpClientException = (HttpClientErrorException) e;
+				throw new ApiNotAccessibleException(httpClientException.getResponseBodyAsString());
+			} else if (e instanceof HttpServerErrorException) {
+				HttpServerErrorException httpServerException = (HttpServerErrorException) e;
+				throw new ApiNotAccessibleException(httpServerException.getResponseBodyAsString());
+			} else {
+				throw new PolicyException(e);
+			}
+
+		}
 
 	}
 
