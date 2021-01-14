59ade9417b71c72bdd47e4e1188739ab828c051b
ase-101 <>
2021-01-15 02:18:58 +0530

    Removed hardcoded fileappender logger config

--- data-share/data-share-service/src/main/java/io/mosip/datashare/logger/DataShareLogger.java
+++ data-share/data-share-service/src/main/java/io/mosip/datashare/logger/DataShareLogger.java
@@ -5,21 +5,6 @@ import io.mosip.kernel.logger.logback.appender.RollingFileAppender;
 import io.mosip.kernel.logger.logback.factory.Logfactory;
 
 public class DataShareLogger {
-	/** The mosip rolling file appender. */
-	private static RollingFileAppender mosipRollingFileAppender;
-
-	static {
-		mosipRollingFileAppender = new RollingFileAppender();
-		mosipRollingFileAppender.setAppend(true);
-		mosipRollingFileAppender.setAppenderName("fileappender");
-		mosipRollingFileAppender.setFileName("logs/datashare.log");
-		mosipRollingFileAppender.setFileNamePattern("logs/datashare-%d{yyyy-MM-dd}-%i.log");
-		mosipRollingFileAppender.setImmediateFlush(true);
-		mosipRollingFileAppender.setMaxFileSize("1mb");
-		mosipRollingFileAppender.setMaxHistory(3);
-		mosipRollingFileAppender.setPrudent(false);
-		mosipRollingFileAppender.setTotalCap("10mb");
-	}
 
 	/**
 	 * Instantiates a new reg processor logger.
@@ -34,6 +19,6 @@ public class DataShareLogger {
 	 * @return the logger
 	 */
 	public static Logger getLogger(Class<?> clazz) {
-		return Logfactory.getDefaultRollingFileLogger(mosipRollingFileAppender, clazz);
+		return Logfactory.getSlf4jLogger(clazz);
 	}
 }
