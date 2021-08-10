e89f84181f44cefa257ad1eaafaf176bedec9631
kameshsr <kameshsr1338@gmail.com>
2021-08-10 17:33:20 +0530

    MOSIP-15288: upgraded swagger2 to openapi3.0 and generated openapi.json in target folder for openapi3.0.
    Resolved conflict

--- data-share/data-share-service/src/main/java/io/mosip/datashare/api/config/DataShareServiceConfig.java
+++ data-share/data-share-service/src/main/java/io/mosip/datashare/api/config/DataShareServiceConfig.java
@@ -1,16 +1,12 @@
 package io.mosip.datashare.api.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
-@EnableSwagger2
 public class DataShareServiceConfig {
 
 	/**
@@ -19,10 +15,11 @@ public class DataShareServiceConfig {
 	 * @return the docket
 	 */
 	@Bean
-	public Docket dataShareapiBean() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("io.mosip.datashare.controller"))
-				.paths(PathSelectors.regex("(?!/(error|actuator).*).*")).build();
-
+	public OpenAPI dataShareapiBean() {
+		return new OpenAPI()
+				.info(new Info().title("Api Documentation")
+						.description("Api Documentation")
+						.version("v0.0.1")
+						.license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0")));
 	}
 }
