a727d77997b52e47532cb5c804333b3e277f3086
kameshsr <kameshsr1338@gmail.com>
2021-10-08 11:49:58 +0530

    MOSIP-15288: Removed hard coded value and added custom server

--- data-share/data-share-service/src/main/java/io/mosip/datashare/api/config/DataShareServiceConfig.java
+++ data-share/data-share-service/src/main/java/io/mosip/datashare/api/config/DataShareServiceConfig.java
@@ -1,25 +1,57 @@
 package io.mosip.datashare.api.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class DataShareServiceConfig {
 
-	/**
-	 * DataShareServiceConfig
-	 *
-	 * @return the docket
-	 */
+	private static final Logger logger = LoggerFactory.getLogger(DataShareServiceConfig.class);
+
+	@Autowired
+	private OpenApiProperties openApiProperties;
+
 	@Bean
-	public OpenAPI dataShareapiBean() {
-		return new OpenAPI()
-				.info(new Info().title("Api Documentation")
-						.description("Api Documentation")
-						.version("v0.0.1")
-						.license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0")));
+	public OpenAPI openApi() {
+		String msg = "Swagger open api, ";
+		OpenAPI api = new OpenAPI()
+				.components(new Components());
+		if (null != openApiProperties.getInfo()) {
+			api.info(new Info()
+					.title(openApiProperties.getInfo().getTitle())
+					.version(openApiProperties.getInfo().getVersion())
+					.description(openApiProperties.getInfo().getDescription()));
+			if (null != openApiProperties.getInfo().getLicense()) {
+				api.getInfo().license(new License()
+						.name(openApiProperties.getInfo().getLicense().getName())
+						.url(openApiProperties.getInfo().getLicense().getUrl()));
+				logger.info(msg + "info license property is added");
+			} else {
+				logger.error(msg + "info license property is empty");
+			}
+			logger.info(msg + "info property is added");
+		} else {
+			logger.error(msg + "info property is empty");
+		}
+
+		if (null != openApiProperties.getDataShareServiceServer().getServers()) {
+			openApiProperties.getDataShareServiceServer().getServers().forEach(server -> {
+				api.addServersItem(new Server().description(server.getDescription()).url(server.getUrl()));
+			});
+			logger.info(msg + "server property is added");
+		} else {
+			logger.error(msg + "server property is empty");
+		}
+		return api;
 	}
+
 }
