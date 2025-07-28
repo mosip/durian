a5cf553edd0d0b3c27a291b096380f9738cfd20b
Gokulraj C <110164849+GOKULRAJ136@users.noreply.github.com>
2025-07-28 16:29:47 +0530

    Change in rest template (#258)
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- data-share/data-share-service/src/main/java/io/mosip/datashare/util/RestUtil.java
+++ data-share/data-share-service/src/main/java/io/mosip/datashare/util/RestUtil.java
@@ -61,13 +61,9 @@ public class RestUtil {
     /** The environment. */
     @Autowired
     private Environment environment;
-    /** The Constant AUTHORIZATION. */
-    private static final String AUTHORIZATION = "Authorization=";
-    private RestTemplate localRestTemplate;
-    @PostConstruct
-    private void loadRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-        localRestTemplate = getRestTemplate();
-    }
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${mosip.data.share.restTemplate.max-connection-per-route:20}")
     private int maxConnectionPerRoute;
@@ -113,9 +109,8 @@ public class RestUtil {
                 }
             }
             try {
-                localRestTemplate = getRestTemplate();
-                result = (T) localRestTemplate.postForObject(builder.toUriString(), setRequestHeader(requestType, mediaType),
-                        responseClass);
+                result = (T) restTemplate.postForObject(
+                        builder.toUriString(), setRequestHeader(requestType, mediaType), responseClass);
             } catch (Exception e) {
                 throw new ApiNotAccessibleException(e);
             }
@@ -159,10 +154,8 @@ public class RestUtil {
             }
             uriComponents = builder.build(false).encode();
             try {
-                localRestTemplate = getRestTemplate();
-                result = (T) localRestTemplate
-                        .exchange(uriComponents.toUri(), HttpMethod.GET, setRequestHeader(null, null), responseType)
-                        .getBody();
+                result = (T) restTemplate.exchange(
+                        uriComponents.toUri(), HttpMethod.GET, setRequestHeader(null, null), responseType).getBody();
             } catch (Exception e) {
                 throw new ApiNotAccessibleException(e);
             }
@@ -179,43 +172,16 @@ public class RestUtil {
             builder = UriComponentsBuilder.fromUriString(apiHostIpPort);
             URI urlWithPath = builder.build(pathsegments);
             try {
-                localRestTemplate = getRestTemplate();
-                result = (T) localRestTemplate
-                        .exchange(urlWithPath, HttpMethod.GET, setRequestHeader(null, null), responseType).getBody();
+                result = (T) restTemplate.exchange(
+                        builder.build(pathsegments), HttpMethod.GET, setRequestHeader(null, null), responseType
+                ).getBody();
             } catch (Exception e) {
                 throw new Exception(e);
             }
         }
         return result;
     }
-    /**
-     * Gets the rest template.
-     *
-     * @return the rest template
-     * @throws KeyManagementException   the key management exception
-     * @throws NoSuchAlgorithmException the no such algorithm exception
-     * @throws KeyStoreException        the key store exception
-     */
-    public RestTemplate getRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-        if (localRestTemplate == null) {
-            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
 
-            SSLContext sslContext = SSLContexts.custom()
-                    .loadTrustMaterial(null, acceptingTrustStrategy).build();
-
-            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-
-            HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create().setSSLSocketFactory(csf).setMaxConnPerRoute(maxConnectionPerRoute).setMaxConnTotal(totalMaxConnection).build();
-
-            CloseableHttpClient httpClient = HttpClients.custom()
-                    .setConnectionManager(connectionManager)
-                    .build();
-
-            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-            localRestTemplate = new RestTemplate(requestFactory);
-        }
-        return localRestTemplate;
-    }
     /**
      * Sets the request header.
      *
@@ -226,7 +192,6 @@ public class RestUtil {
      */
     private HttpEntity<Object> setRequestHeader(Object requestType, MediaType mediaType) throws IOException {
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-        headers.add("Cookie", getToken());
         if (mediaType != null) {
             headers.add("Content-Type", mediaType.toString());
         }
@@ -250,80 +215,5 @@ public class RestUtil {
             }
         } else
             return new HttpEntity<Object>(headers);
-    }
-    /**
-     * Gets the token.
-     *
-     * @return the token
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-    public String getToken() throws IOException {
-        String token = System.getProperty("token");
-        boolean isValid = false;
-        if (StringUtils.isNotEmpty(token)) {
-            isValid = TokenHandlerUtil.isValidBearerToken(token,
-                    environment.getProperty("data.share.token.request.issuerUrl"),
-                    environment.getProperty("data.share.token.request.clientId"));
-        }
-        if (!isValid) {
-            TokenRequestDTO<SecretKeyRequest> tokenRequestDTO = new TokenRequestDTO<SecretKeyRequest>();
-            tokenRequestDTO.setId(environment.getProperty("data.share.token.request.id"));
-            tokenRequestDTO.setMetadata(new Metadata());
-            tokenRequestDTO.setRequesttime(DateUtils.getUTCCurrentDateTimeString());
-            // tokenRequestDTO.setRequest(setPasswordRequestDTO());
-            tokenRequestDTO.setRequest(setSecretKeyRequestDTO());
-            tokenRequestDTO.setVersion(environment.getProperty("data.share.token.request.version"));
-
-            Gson gson = new Gson();
-            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-            // HttpPost post = new
-            // HttpPost(environment.getProperty("PASSWORDBASEDTOKENAPI"));
-            HttpPost post = new HttpPost(environment.getProperty("KEYBASEDTOKENAPI"));
-            try {
-                StringEntity postingString = new StringEntity(gson.toJson(tokenRequestDTO));
-                post.setEntity(postingString);
-                post.setHeader("Content-type", "application/json");
-                CloseableHttpResponse response = httpClient.execute(post);
-
-
-                org.apache.hc.core5.http.HttpEntity entity = response.getEntity();
-                String responseBody = EntityUtils.toString(entity, "UTF-8");
-                Header[] cookie = response.getHeaders("Set-Cookie");
-                if (cookie.length == 0)
-                    throw new IOException("cookie is empty. Could not generate new token.");
-                token = response.getHeaders("Set-Cookie")[0].getValue();
-                System.setProperty("token", token.substring(14, token.indexOf(';')));
-                return token.substring(0, token.indexOf(';'));
-            } catch (IOException e) {
-                throw e;
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return AUTHORIZATION + token;
-    }
-    /**
-     * Sets the secret key request DTO.
-     *
-     * @return the secret key request
-     */
-    private SecretKeyRequest setSecretKeyRequestDTO() {
-        SecretKeyRequest request = new SecretKeyRequest();
-        request.setAppId(environment.getProperty("data.share.token.request.appid"));
-        request.setClientId(environment.getProperty("data.share.token.request.clientId"));
-        request.setSecretKey(environment.getProperty("data.share.token.request.secretKey"));
-        return request;
-    }
-    /**
-     * Sets the password request DTO.
-     *
-     * @return the password request
-     */
-    private PasswordRequest setPasswordRequestDTO() {
-        PasswordRequest request = new PasswordRequest();
-        request.setAppId(environment.getProperty("data.share.token.request.appid"));
-        request.setPassword(environment.getProperty("data.share.token.request.password"));
-        request.setUserName(environment.getProperty("data.share.token.request.username"));
-        return request;
     }
 }
