7be651fcfd22da8c70edcf409e8a72ad5f45ea0d
Dhanendra Sahu <dhanendra.tech@gmail.com>
2025-09-24 11:20:26 +0530

    fix the authentication failed issue
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- data-share/data-share-service/src/main/java/io/mosip/datashare/dto/AuthorizedRolesDTO.java
+++ data-share/data-share-service/src/main/java/io/mosip/datashare/dto/AuthorizedRolesDTO.java
@@ -16,6 +16,6 @@ import lombok.Setter;
 public class AuthorizedRolesDTO {
 
 
-    private List<String> postcreatepolicyidsubscriberid;
+    private List<String> postcreatepolicyIdsubscriberId;
     
 }
