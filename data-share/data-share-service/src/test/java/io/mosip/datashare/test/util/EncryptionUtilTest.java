b414051d2de04e76b0274e1b5902a0f721229416
Dhanendra Sahu <dhanendra.tech@gmail.com>
2025-10-28 11:30:47 +0530

    Fix the test case
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- data-share/data-share-service/src/test/java/io/mosip/datashare/test/util/EncryptionUtilTest.java
+++ data-share/data-share-service/src/test/java/io/mosip/datashare/test/util/EncryptionUtilTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
